ee1fd7c985421bde84a47b0502a7b823940216c2
Taylor Fox Dahlin <dahlit@rpi.edu>
2015-10-09 21:47:30 -0400

    Comments! Yay!

--- client/client.py
+++ client/client.py
@@ -58,6 +58,7 @@ if __name__ == "__main__":
 	#then after that we determine what the program start time is
 	clientstarttime = time.time()
 
+
 	UDPsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	UDPsock.bind(('',1392))
 	
@@ -67,29 +68,31 @@ if __name__ == "__main__":
 
 	heartbeatthread.start()
 	listenthread.start()
+	
+	try:
+		while (1):
+			#because it is used multiple times, there's no reason to continue
+			#to call the time function, so instead it is just stored into a
+			#variable at the beginning of the while loop
+			currenttime = time.time()
 
-	while (1):
-#		meme = UDPsock.recvfrom(1024)
-#		print(meme[0])
-		#because it is used multiple times, there's no reason to continue
-		#to call the time function, so instead it is just stored into a
-		#variable at the beginning of the while loop
-		currenttime = time.time()
+			#this will be replaced eventually, and there'll be some way to
+			#determine user activity
+			idletime = currenttime - clientstarttime
 
-		#this will be replaced eventually, and there'll be some way to
-		#determine user activity
-		idletime = currenttime - clientstarttime
-
-		#Heartbeat function runs every 3 seconds this way.
-		if( idletime > 1200 ):
-			heartbeatthread.pause()
-			close = input("Would you like to continue?[Y/N]")
-			while( (close.lower() != "n") & (close.lower() != "y")
-							& (close.lower() != "yes") & (close.lower() != "no") ):
-				close = input("Invalid input.\nWould you like to continue?[Y/N]")
-			if( (close.lower() == "n") | (close.lower() == "no") ):
-				print("Client timeout.")
-				os._exit(0)
-			else:
-				heartbeatthread.resume()
-				clientstarttime = time.time()
+			#Heartbeat function runs every 3 seconds this way.
+			if( idletime > 1200 ):
+				heartbeatthread.pause()
+				close = input("Would you like to continue?[Y/N]")
+				while( (close.lower() != "n") & (close.lower() != "y")
+								& (close.lower() != "yes") & (close.lower() != "no") ):
+					close = input("Invalid input.\nWould you like to continue?[Y/N]")
+				if( (close.lower() == "n") | (close.lower() == "no") ):
+					print("Client timeout.")
+					os._exit(0)
+				else:
+					heartbeatthread.resume()
+					clientstarttime = time.time()
+	except KeyboardInterrupt:
+		print("")
+		os._exit(0)
